0fb32550ed8dda25bfc7a743bf5bc79fbad9abdd
EmanuelfrJunior <emanuelfrjunior@gmail.com>
2025-11-19 22:36:36 -0300

    atualização-preparo-campo-de-mensagem

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -3,8 +3,9 @@ export function SectionComponents(){
     return(
         <>
         <div className="bg-white w-full h-screen flex itens-center justify-center pt-[8%]">
-            <div className="bg-gray-100 w-[70%] h-[90%]">
-                chat rick sanchez pelo OpenIA
+            <div className="bg-gray-100 w-[70%] h-[90%] rounded-lg p-2">
+                <div className="bg-white h-[89%] rounded-xl mb-2">chat rick sanchez pelo OpenIA</div>
+                <div className="bg-white h-[10%] rounded-xl"><input type="text" name="" id="" /></div>
             </div>
         </div>
         </>
