4c3e6acfe38b41d4193ad0a378dd2296bfc45142
EmanuelfrJunior <emanuelfrjunior@gmail.com>
2025-11-20 21:39:49 -0300

    implementação-do-forms-para-o-botão

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -2,13 +2,15 @@
 export function SectionComponents(){
     return(
         <>
-        <div className="bg-white w-full h-screen flex itens-center justify-center pt-[8%]">
+        <div className="bg-white w-full h-screen flex itens-center justify-center pt-[8%]" id="slime-top">
             <div className="bg-gray-100 w-[70%] h-[90%] rounded-lg p-2">
                 <div className="bg-white h-[89%] rounded-xl mb-2">chat rick sanchez pelo OpenIA</div>
                 <div className=" w-full h-[10%] flex itens-center justify-center">
                     <div className="bg-white w-[90%] h-[100%] rounded-xl flex"><input type="text" name="" id="" /></div>
                     <div className=" bg-green-500 w-[5%] ml-2 rounded-2xl flex itens-center justify-center p-3">
-                        <button type="submit"><img src="src/assets/seta.png" alt="" /></button>
+                        <form action="" method="post">
+                            <button className="cursor-pointer" type="submit"><img src="src/assets/seta.png" alt="" /></button>
+                        </form>
                     </div>
                 </div>
             </div>
